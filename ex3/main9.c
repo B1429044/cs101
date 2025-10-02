13c4cbb84fc9f25fa89143cd117c230a0432b195
B1429044 <B1429044@cgu.edu.tw>
2025-10-02 15:36:32 +0800

    Create main9.c

---
+++ ex3/main9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = -9;
+    int y = 0;
+    int z = 9;
+    if(x < 0){
+        int sum = (x*-1)*100 + y*10 + z;
+        printf("%d", sum*-1);
+    }
+    else {
+        printf("%d", x*100 + y*10 + z);
+    }
+    return 0;
+}
