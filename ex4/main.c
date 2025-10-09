de300229f827c8d18dad74097b69519ecd6c82f6
B1429044 <B1429044@cgu.edu.tw>
2025-10-09 15:55:43 +0800

    Update main.c

--- ex4/main.c
+++ ex4/main.c
@@ -5,23 +5,21 @@ int main() {
     FILE* fp;
     int a[] = {0,1,2};
     char b[] = "ABC";
-    float c[] = {1,1,1,2,1,3};
+    float c[] = {1.1,1.2,1.3};
     int a_read[3];
     char b_read[4];
-    float c_read[6];
+    float c_read[3];
     
     fp = fopen("a.bin", "wb+");
     fwrite(a, sizeof(int), 3, fp);
     fwrite(b, sizeof(char), 4, fp);
-    fwrite(c, sizeof(float), 6, fp);
+    fwrite(c, sizeof(float), 3, fp);
     
-    rewind(fp);
+    fseek(fp, 0, SEEK_SET);
 
     fread(a_read, sizeof(int), 3, fp);
     fread(b_read, sizeof(char), 4, fp);
-    fread(c_read, sizeof(float), 6, fp);
-
-    fclose(fp);
+    fread(c_read, sizeof(float), 3, fp);
     
     for (int i = 0; i < 3; i++) {
         printf("%d ", a_read[i]);
@@ -30,7 +28,7 @@ int main() {
 
     printf("%s\n", b_read);
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 3; i++) {
         printf("%f ", c_read[i]);
     }
     printf("\n");
